98a3405c03296f81036cb783b1e1183e7551a0ac
Sarah Park <jhpark981014@gmail.com>
2023-06-21 13:14:51 +0900

    Clean-Up

--- src/App.js
+++ src/App.js
@@ -3,39 +3,31 @@
 
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byeFn() {
+    console.log("bye :)");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byeFn(); // byeFn()을 쓰지 않는다
+  }
+  useEffect(hiFn, []);
+
+  // useEffect(() => {
+  //   console.log("created :)");
+  //   // return () => console.log("destroyed :(");
+  // }, []);
+
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1); // 버튼에 onClick eventListner로 설정
-  const onChange = (event) => setKeyword(event.target.value); // input에 onChange eventListner로 설정
-  // console.log("I run all the time");
-
-  const runOnlyOnce = () => {
-    console.log("I run only once");
-  };
-  useEffect(runOnlyOnce, []);
-  useEffect(() => {
-    if (keyword != "" && keyword.length > 5) {
-      console.log("Search for", keyword);
-    }
-  }, [keyword]); // keyword가 바뀌면, 코드를 실행해!
-
-  useEffect(() => {
-    console.log("keyword or counter has changed");
-  }, [keyword, counter]); // keyword 혹은 counter가 바뀌면, 코드를 실행해!
-
-  // console.log("Search for", keyword);
-
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here ..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click Me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
